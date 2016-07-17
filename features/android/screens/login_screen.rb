cd3640f2b106afdb69fe98bbacb92e9a0acc6556
Wellington <wellington.santos@concrete.com.br>
2016-07-17 13:23:43 -0300

    Adiciona feature login

---
+++ features/android/screens/login_screen.rb
@@ -0,0 +1,13 @@
+class LoginScreen < AndroidScreenBase
+  # Identificador da tela
+  # trait(:trait)                 { "* id:'#{layout_name}'" }
+
+  # Declare todos os elementos da tela
+  # element(:layout_name)         { pending 'Insert layout identificator' }
+  # element(:button)              { pending 'Insert button identificator' }
+
+  # Declare todas as acoes da tela
+  # action(:touch_button) do
+  #   touch("* id:'#{button}'")
+  # end
+end
---
+++ features/login.feature
@@ -0,0 +1,8 @@
+# language: pt
+Funcionalidade: Login 
+
+  Contexto:
+    # Insira os passos
+    
+  Cenário: Primeiro Cenário
+    # Insira os passos
